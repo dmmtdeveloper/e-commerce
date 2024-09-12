4af2e5aa7b424ccdb37f5c56221a25e2457c76ee
CarlosZuritaEstolaza <carlos.zurita.inf@gmail.com>
2024-09-12 01:28:05 -0300

    agregando los metodos de usuario para la pagina de configuracion

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,77 +2,328 @@
 import { useState, useEffect } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { NavSetting } from "@/components/shared/NavSetting";
-import { GetUsuarioByToken } from "@/utils/authHelpers"; // Ajusta el path correctamente
+import { GetUsuarioByToken, UpdateNombreUsuario, UpdateClaveUsuario } from "@/utils/authHelpers"; // Ajusta el path correctamente
 import { Usuario } from "@/utils/authHelpers"; // Asegúrate de importar la interfaz correctamente
+import { useAuthStore } from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
 
 export default function SettingsPage() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [nuevaPassword, setNuevaPassword] = useState<string>("");
+
   const [usuario, setUsuario] = useState<Usuario | null>(null);
+  const [nombre, setNombre] = useState<string>("");
+  const [correo, setCorreo] = useState<string>("");
+  const [clave, setClave] = useState<string>("");
+  const [habilitado, setHabilitado] = useState<boolean>(false);
+  const [eliminado, setEliminado] = useState<boolean>(false);
+  const [esAdmin, setEsAdmin] = useState<boolean>(false);
+
+  const { logout } = useAuthStore();
+  const router = useRouter();
+
+  // Estado para manejar la visibilidad del modal
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [confirmAction, setConfirmAction] = useState<(() => void) | null>(null);
+  const [modalMessage, setModalMessage] = useState<string>("");
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState<boolean>(false);
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const token = sessionStorage.getItem("token");
     if (token) {
-      // Si el token existe, hacemos la llamada al servicio para obtener el usuario
       GetUsuarioByToken(token)
         .then((usuario) => {
           setUsuario(usuario);
-          setEmail(usuario.correo); // Asigna el correo obtenido
-          setPassword(usuario.clave); // Asigna la clave obtenida
+          setNombre(usuario.nombre);
+          setCorreo(usuario.correo);
+          setClave(usuario.clave);
+          setHabilitado(usuario.habilitado);
+          setEliminado(usuario.eliminado);
+          setEsAdmin(usuario.esAdmin);
         })
         .catch((error) => {
           console.error("Error obteniendo el usuario:", error);
         });
-        console.log("id" + usuario?.usuarioId)
     } else {
       console.warn("Token no encontrado en sessionStorage");
     }
-  }, [usuario?.usuarioId]); // Se ejecuta una sola vez al montar el componente
+  }, []); // Se ejecuta una sola vez al montar el componente
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Lógica para guardar los cambios, usando los estados email, password y nuevaPassword
-    console.log("Guardar cambios con:", { email, password, nuevaPassword });
+  const handleUpdate = async (field: string) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      try {
+        console.log(`Actualizando ${field}:`, {
+          nombre,
+          correo,
+          clave,
+          habilitado,
+          eliminado,
+          esAdmin,
+        });
+        setModalMessage(`El campo ${field} se actualizó correctamente.`);
+        openConfirmationModal(() => {}, `¿Estás seguro de que quieres actualizar el ${field}?`);
+      } catch (error) {
+        console.error("Error actualizando el usuario:", error);
+      }
+    } else {
+      console.warn("Token no encontrado en sessionStorage");
+    }
   };
+
+  const handleDeleteAccount = async () => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      try {
+        // await axios.delete("/api/Usuario/eliminar", {
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //   },
+        // });
+        logout();
+        router.push("/login");
+      } catch (error) {
+        console.error("Error eliminando la cuenta:", error);
+      }
+    } else {
+      console.warn("Token no encontrado en sessionStorage");
+    }
+  };
+
+  const openConfirmationModal = (action: () => void, message: string = "¿Estás seguro de que quieres realizar esta acción?") => {
+    setConfirmAction(() => action);
+    setModalMessage(message);
+    setIsModalOpen(true);
+  };
+
+  const closeConfirmationModal = () => {
+    setIsModalOpen(false);
+    setConfirmAction(() => null); // Usa una función vacía para evitar el error de tipo
+    setModalMessage("");
+  };
+
+  const confirmActionHandler = () => {
+    if (confirmAction) {
+      confirmAction();
+      closeConfirmationModal();
+    }
+  };
+
+  const handleUpdateName = async () => {
+    const id = usuario?.usuarioId;
+  
+    // Validar que el nombre no esté vacío
+    if (nombre.trim().length === 0) {
+      openErrorModal("El nombre no puede estar vacío.");
+      return; // Termina la función si el nombre no es válido
+    }
+  
+    if (id) {
+      try {
+        await UpdateNombreUsuario(id, nombre);
+        openSuccessModal("Nombre actualizado correctamente.");
+      } catch (error) {
+        console.error("Error actualizando el nombre:", error);
+        openErrorModal("Error actualizando el nombre.");
+      }
+    } else {
+      console.warn("Id no existe");
+    }
+  };
+  
+  
+  const handleUpdateClave = async () => {
+    const id = usuario?.usuarioId;
+  
+    // Validar que la clave tenga al menos 6 caracteres
+    if (clave.length < 6) {
+      openErrorModal("La clave debe tener al menos 6 caracteres.");
+      return; // Termina la función si la clave no es válida
+    }
+  
+    if (id) {
+      try {
+        await UpdateClaveUsuario(id, clave);
+        openSuccessModal("Clave actualizada correctamente.");
+      } catch (error) {
+        console.error("Error actualizando la clave:", error);
+        openErrorModal("Error actualizando la clave.");
+      }
+    } else {
+      console.warn("Id no existe");
+    }
+  };
+  
+  
+
+  if (!usuario) {
+    return <div>Cargando...</div>;
+  }
+
+  const openSuccessModal = (message: string) => {
+    setModalMessage(message);
+    setIsSuccessModalOpen(true);
+  };
+
+  const openErrorModal = (message: string) => {
+    setModalMessage(message);
+    setIsErrorModalOpen(true);
+  };
+
+  const closeSuccessModal = () => setIsSuccessModalOpen(false);
+  const closeErrorModal = () => setIsErrorModalOpen(false);
 
   return (
     <MainLayout>
       <section className="p-4 mt-32 gap-10 flex flex-col">
         <NavSetting />
-
+  
         <div>
           <h1 className="font-semibold text-4xl">Tu Configuración</h1>
           <p>Actualizar la configuración de tu cuenta</p>
         </div>
-
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Correo"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-2"
-          />
-          <input
-            type="password"
-            placeholder="Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-2"
-          />
-          <input
-            type="password"
-            placeholder="Nueva contraseña"
-            value={nuevaPassword}
-            onChange={(e) => setNuevaPassword(e.target.value)}
-            className="border p-2"
-          />
-          <button type="submit" className="bg-blue-500 text-white p-2">
-            Guardar cambios
+  
+        <table className="w-full border">
+          <tbody>
+            <tr>
+              <td className="p-2">Usuario ID:</td>
+              <td className="p-2">
+                <input
+                  type="text"
+                  value={usuario?.usuarioId}
+                  readOnly
+                  className="border p-2 bg-gray-200"
+                />
+              </td>
+              <td className="p-2"></td>
+            </tr>
+            <tr>
+              <td className="p-2">Correo:</td>
+              <td className="p-2">
+                <input
+                  type="text"
+                  value={correo}
+                  readOnly
+                  className="border p-2 bg-gray-200"
+                />
+              </td>
+              <td className="p-2"></td>
+            </tr>
+            <tr>
+              <td className="p-2">Nombre:</td>
+              <td className="p-2">
+                <input
+                  type="text"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                  className="border p-2"
+                />
+              </td>
+              <td className="p-2">
+                <button
+                  type="button"
+                  onClick={() => openConfirmationModal(() => handleUpdateName(), "¿Estás seguro de que quieres actualizar el nombre?")}
+                  className="bg-blue-500 text-white p-2"
+                >
+                  Actualizar Nombre
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="p-2">Clave:</td>
+              <td className="p-2">
+                <input
+                  type="text"
+                  value={clave}
+                  onChange={(e) => setClave(e.target.value)}
+                  className="border p-2"
+                />
+              </td>
+              <td className="p-2">
+                <button
+                  type="button"
+                  onClick={() => openConfirmationModal(() => handleUpdateClave(), "¿Estás seguro de que quieres actualizar la clave?")}
+                  className="bg-blue-500 text-white p-2"
+                >
+                  Actualizar Clave
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  
+        <div className="mt-4">
+          <button
+            type="button"
+            onClick={() => openConfirmationModal(() => handleDeleteAccount(), "¿Estás seguro de que quieres eliminar tu cuenta?")}
+            className="bg-red-500 text-white p-2"
+          >
+            Eliminar Cuenta
           </button>
-        </form>
+        </div>
+  
+        {/* Modal de Confirmación */}
+        {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <div className="bg-white p-6 rounded shadow-lg">
+              <h2 className="text-xl font-semibold mb-4">Confirmación</h2>
+              <p>{modalMessage}</p>
+              <div className="mt-4 flex justify-end gap-4">
+                <button
+                  onClick={confirmActionHandler}
+                  className="bg-blue-500 text-white p-2 rounded"
+                >
+                  Confirmar
+                </button>
+                <button
+                  onClick={closeConfirmationModal}
+                  className="bg-gray-500 text-white p-2 rounded"
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+  
+        {/* Modal de Éxito */}
+        {isSuccessModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-green-800 bg-opacity-50">
+            <div className="bg-white p-6 rounded shadow-lg">
+              <h2 className="text-xl font-semibold mb-4">Éxito</h2>
+              <p>{modalMessage}</p>
+              <div className="mt-4 flex justify-end">
+                <button
+                  onClick={closeSuccessModal}
+                  className="bg-green-500 text-white p-2 rounded"
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+  
+        {/* Modal de Error */}
+        {isErrorModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-red-800 bg-opacity-50">
+            <div className="bg-white p-6 rounded shadow-lg">
+              <h2 className="text-xl font-semibold mb-4">Error</h2>
+              <p>{modalMessage}</p>
+              <div className="mt-4 flex justify-end">
+                <button
+                  onClick={closeErrorModal}
+                  className="bg-red-500 text-white p-2 rounded"
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </section>
     </MainLayout>
-  );
+  );  
 }
--- src/utils/authHelpers.ts
+++ src/utils/authHelpers.ts
@@ -1,6 +1,7 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import axiosInstance from "./axiosInstance"; // Importa tu instancia de axios
 import jwt from "jsonwebtoken";
+import axios from 'axios';
 
 export const register = async (
   nombre: string,
@@ -49,6 +50,107 @@ export const GetUsuarioByToken = async (token: string): Promise<Usuario> => {
   } catch (error) {
     console.error("Error obteniendo el usuario:", error);
     throw error;
+  }
+};
+
+//metodo put para actualizar nombre
+export const UpdateNombreUsuario = async (usuarioId: number, nombre: string): Promise<void> => {
+  try {
+    // Enviamos la solicitud PUT
+    const response = await axiosInstance.put<void>(
+      `/api/Usuario/update/name/${usuarioId}/${nombre}`
+    );
+
+    // Verificamos el código de estado HTTP
+    if (response.status === 204) {
+      // Actualización exitosa
+      console.log("Nombre actualizado con éxito.");
+    } else {
+      // Código de estado inesperado
+      throw new Error("Respuesta inesperada del servidor.");
+    }
+  } catch (error) {
+
+    handleError(error);
+  }
+};
+
+export const UpdateClaveUsuario = async (usuarioId: number, clave: string): Promise<void> => {
+  try {
+    const response = await axiosInstance.put<void>(
+      `/api/Usuario/update/password/${usuarioId}/${clave}`
+    );
+
+    if (response.status === 204) {
+      console.log("Clave actualizada con éxito.");
+    } else {
+      throw new Error("Respuesta inesperada del servidor.");
+    }
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const UpdateHabilitadoUsuario = async (usuarioId: number, habilitado: boolean): Promise<void> => {
+  try {
+    const response = await axiosInstance.put<void>(
+      `/api/Usuario/update/enabled/${usuarioId}/${habilitado}`
+    );
+
+    if (response.status === 204) {
+      console.log("Estado de habilitado actualizado con éxito.");
+    } else {
+      throw new Error("Respuesta inesperada del servidor.");
+    }
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const UpdateEliminadoUsuario = async (usuarioId: number, eliminado: boolean): Promise<void> => {
+  try {
+    const response = await axiosInstance.put<void>(
+      `/api/Usuario/update/deleted/${usuarioId}/${eliminado}`
+    );
+
+    if (response.status === 204) {
+      console.log("Estado de eliminado actualizado con éxito.");
+    } else {
+      throw new Error("Respuesta inesperada del servidor.");
+    }
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const UpdateEsAdminUsuario = async (usuarioId: number, esAdmin: boolean): Promise<void> => {
+  try {
+    const response = await axiosInstance.put<void>(
+      `/api/Usuario/update/esadmin/${usuarioId}/${esAdmin}`
+    );
+
+    if (response.status === 204) {
+      console.log("Estado de administrador actualizado con éxito.");
+    } else {
+      throw new Error("Respuesta inesperada del servidor.");
+    }
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+const handleError = (error: any) => {
+  if (axios.isAxiosError(error) && error.response) {
+    if (error.response.status === 500) {
+      console.error("Error interno del servidor:", error);
+      throw new Error("Error interno del servidor.");
+    } else {
+      console.error("Error en la solicitud:", error);
+      throw new Error("Error en la solicitud.");
+    }
+  } else {
+    console.error("Error desconocido:", error);
+    throw new Error("Error desconocido.");
   }
 };
 
