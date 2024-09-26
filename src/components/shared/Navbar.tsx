d6e1b97f2e57edc6032703042b23609f16a88ab4
unknown <dmmtapia.ux@gmail.com>
2024-09-26 01:02:44 -0300

    navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,33 +1,69 @@
 import Link from "next/link";
-import { useState } from "react";
-import { FaShoppingCart, FaTimes, FaHome } from "react-icons/fa";
+import { useState, useRef, useEffect, MutableRefObject } from "react";
+import { FaShoppingCart, FaHome } from "react-icons/fa";
 import { useAuthStore } from "@/store/useAuthStore";
 import useCartStore from "@/store/cartStore";
 import { useRouter } from "next/navigation";
+import { BiArrowToRight } from "react-icons/bi";
+import { AiOutlineSetting } from "react-icons/ai";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { FaUsers } from "react-icons/fa6";
+import Image from "next/image";
+import userImg from "@/public/assets/img/user.png";
+import { FaBagShopping } from "react-icons/fa6";
+import { AiFillProduct } from "react-icons/ai";
 
-const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const { email, name, isAuthenticated, logout, isAdmin } = useAuthStore();
   const { items } = useCartStore();
   const router = useRouter();
+  const menuRef = useRef<HTMLDivElement | null>(
+    null
+  ) as MutableRefObject<HTMLDivElement | null>;
 
   const totalItemsInCart = items.reduce(
     (total, item) => total + item.quantity,
     0
   );
 
-  const handleLogout = () => {
+  const handleLogout = (): void => {
     logout();
     router.push("/login");
   };
 
+  const handleClickOutside = (event: MouseEvent): void => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect((): (() => void) => {
+    if (menuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuOpen]);
+
   return (
     <nav className="fixed w-full flex justify-between items-center p-4 bg-gray-800 text-white z-[99999]">
-      <Link href="/">
-        <div className="text-2xl font-bold">e-commerce</div>
+      <Link href="/" className="text-2xl font-bold">
+        e-commerce
       </Link>
-      <div className="flex gap-4 items-center">
-        {/* Menú principal */}
+
+      <section className="flex gap-4 items-center">
+        {!isAuthenticated && (
+          <>
+            <Link href="/register">Register</Link>
+            <Link href="/login">Login</Link>
+          </>
+        )}
+
         <Link href="/cart" className="relative">
           <FaShoppingCart />
           {totalItemsInCart > 0 && (
@@ -40,77 +76,83 @@ const Navbar = () => {
           <FaHome />
         </Link>
 
-        {/* Links de autenticación */}
-        {!isAuthenticated ? (
-          <>
-            <Link href="/register">Register</Link>
-            <Link href="/login" className="cursor-pointer">
-              Login
-            </Link>
-          </>
-        ) : (
-          <>
-            <button
-              onClick={handleLogout}
-              className="bg-red-500 text-white px-2 py-2 rounded"
-            >
-              Cerrar sesión
-            </button>
-          </>
-        )}
-
-        {/* Menú desplegable */}
-        <div className="relative">
+        {/* Modal de usuario */}
+        <div className="relative" ref={menuRef}>
           <span
             onClick={() => setMenuOpen(!menuOpen)}
             className="cursor-pointer"
           >
-            {name || email} {/* Mostrar nombre si está disponible, de lo contrario el email */}
+            {name || email}
           </span>
 
-          {menuOpen && isAuthenticated && (
-            <div className="absolute right-0 mt-2 bg-white text-black p-4 rounded shadow-lg">
-              <div className="flex justify-between items-center">
-                <div>
-                  <p>{name || email}</p> {/* Mostrar nombre o email en el menú */}
+          {/* Modal con transición */}
+          <div
+            className={`absolute right-0 mt-2 bg-white text-black p-4 shadow-lg w-72 rounded-3xl transform transition-transform duration-300 ease-in-out ${
+              menuOpen ? "scale-100 opacity-100" : "scale-0 opacity-0"
+            }`}
+          >
+            <div className="p-4">
+              <article className="flex justify-between items-center bg-slate-300 p-6 rounded-3xl shadow-sm">
+                <div className="flex flex-col items-center justify-center gap-2">
+                  <Image
+                    width={50}
+                    height={50}
+                    priority
+                    src={userImg}
+                    alt="user"
+                  />
+                  <p>{name}</p>
+                  <p>{email}</p>
                 </div>
-                <button onClick={() => setMenuOpen(false)}>
-                  <FaTimes />
-                </button>
-              </div>
-              <br></br>
-              {/* Sección Menú */}
-              <h3 className="text-lg font-semibold">Menú</h3>
-              <ul>
-                <li>
-                  <Link href="/settings">Configuración</Link>
-                </li>
-                <li>
-                  <Link href="/orders">Mis Pedidos</Link>
-                </li>
-              </ul>
-
-              {/* Sección Admin (solo si es administrador) */}
+              </article>
+              <h3 className="text-xl font-semibold mb-4 mt-4">Menú</h3>
+              <article className="flex flex-col gap-4">
+                <ul className="flex flex-col gap-2">
+                  <li className="flex items-center gap-2 hover:text-purple-500 transition-all hover:bg-slate-200 p-2 rounded-xl">
+                    <CiDeliveryTruck className="text-2xl" />
+                    <Link href="/orders">Mis Pedidos</Link>
+                  </li>
+                  <li className="flex items-center gap-2 hover:text-purple-500 transition-all hover:bg-slate-200 p-2 rounded-xl">
+                    <AiOutlineSetting className="text-2xl" />
+                    <Link href="/settings">Configuración</Link>
+                  </li>
+                </ul>
+              </article>
               {isAdmin && (
-                <>
-                  <h3 className="text-lg font-semibold mt-4">Admin</h3>
-                  <ul>
-                    <li>
-                      <Link href="/admin/products">Productos</Link>
-                    </li>
-                    <li>
-                      <Link href="/admin/users">Usuarios</Link>
-                    </li>
-                    <li>
-                      <Link href="/admin/orders">Pedidos</Link>
-                    </li>
-                  </ul>
-                </>
+                <article className="mt-4">
+                  <h3 className="text-xl font-bold mb-4">Administración</h3>
+
+                  <div className="flex flex-col gap-2">
+                    <ul className="flex flex-col gap-2">
+                      <li className="font-normal flex gap-4 hover:text-purple-500 transition-all hover:bg-slate-200 p-2 rounded-xl">
+                        <AiFillProduct className="text-2xl" />
+                        <Link href="/admin/products">Productos</Link>
+                      </li>
+
+                      <li className="flex gap-4 hover:text-purple-500 transition-all hover:bg-slate-200 p-2 rounded-xl">
+                        <FaUsers className="text-2xl" />
+                        <Link href="/admin/users">Usuarios</Link>
+                      </li>
+
+                      <li className="items-center flex gap-4 hover:text-purple-500 transition-all hover:bg-slate-200 p-2 rounded-xl">
+                        <FaBagShopping className="text-2xl" />
+                        <Link href="/admin/orders">Pedidos</Link>
+                      </li>
+                    </ul>
+                    <button
+                      onClick={handleLogout}
+                      className="flex items-center gap-4 text-black hover:text-red-500 transition-all hover:bg-slate-200 p-2 rounded-xl"
+                    >
+                      <BiArrowToRight className="text-2xl" />
+                      Cerrar sesión
+                    </button>
+                  </div>
+                </article>
               )}
             </div>
-          )}
+          </div>
         </div>
-      </div>
+      </section>
     </nav>
   );
 };
