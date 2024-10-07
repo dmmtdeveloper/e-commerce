6b4b325a2ebf4ad4afe10711b91e428f7ead2b36
unknown <dmmtapia.ux@gmail.com>
2024-10-07 20:47:23 -0300

    animate settings stop

--- src/components/modals/setting-modal-component/sucess-modal-component/success-modal-component.tsx
+++ src/components/modals/setting-modal-component/sucess-modal-component/success-modal-component.tsx
@@ -1,6 +1,6 @@
-import Lottie from "lottie-react";
+import Lottie, {LottieRefCurrentProps} from "lottie-react";
 import sucessAnimation from "@/public/assets/animation/Animation-success.json";
-import React from "react";
+import React, { useRef } from "react";
 import ButtonModalClose from "../../button-modal/button-modal-close";
 
 interface SuccessModalProps {
@@ -15,12 +15,15 @@ const SuccessModal: React.FC<SuccessModalProps> = ({
   onClose,
 }) => {
   if (!isOpen) return null;
-
+  
+  const successRef = useRef<LottieRefCurrentProps>(null)
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div className="bg-white p-6 rounded-3xl">
         <div className="flex items-center justify-center">
-          <Lottie className="w-52" animationData={sucessAnimation} />
+          <Lottie onComplete={() => {
+            successRef.current?.goToAndPlay(45, true)
+          }} lottieRef={successRef} loop={false} className="w-52" animationData={sucessAnimation} />
         </div>
         <h2 className="text-xl font-semibold mb-4">Éxito</h2>
         <p className="text-sm">{message}</p>
